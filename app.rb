7c219280299e9413a101d0d5cb9426b7e269cbca
Martine9670 <pinnamartine@free.fr>
2025-06-02 16:10:12 +0200

    add all

--- app.rb
+++ app.rb
@@ -4,11 +4,3 @@ require_relative '/lib/game'
 require_relative '/lib/player'
 require_relative '/lib/show'
 
-class Application
-  def perform
-    # TO DO : méthode qui initialise le jeu puis contient des boucles while pour faire tourner le jeu tant que la partie n'est pas terminée.
-    # 
-
-  end
-
-end
