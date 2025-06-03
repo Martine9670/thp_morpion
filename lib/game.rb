b8359ea6f56970b0f333d8e7dfd279856c3ac75b
Martine9670 <pinnamartine@free.fr>
2025-06-03 11:40:57 +0200

    add all

--- lib/game.rb
+++ lib/game.rb
@@ -32,7 +32,7 @@ class Game
   end
 
   def turn
-    #TO DO : méthode faisant appelle aux méthodes des autres classes (notamment à l'instance de Board). 
+    #TO DO : méthode faisant appel aux méthodes des autres classes (notamment à l'instance de Board). 
             # Elle affiche le plateau, demande au joueur ce qu'il joue, vérifie si un joueur a gagné, 
             # passe au joueur suivant si la partie n'est pas finie.
     Show.new.show_board(@board)                # Affiche le plateau actuel grâce à la classe Show
